f47ce69b8fe247bb862154ff0b675711dfa9fd92
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-03 12:30:51 -0500

    Added commented IIFE lines. Changed move() method to be properly created instead of as a property with a function

--- js/Battleship.js
+++ js/Battleship.js
@@ -1,3 +1,4 @@
+// (function() {
 "use strict"
 
 //
@@ -25,29 +26,31 @@ class Ship {
         this.id = Ship.incrementId();
         this.length = length;
         this.direction = true;
-        this.move = function (locationID, dir) {
-            var loc = getOffset(locationID);
-            console.log("Moving Ship" + this.id + " to " + locationID + " (" + loc.left + ", " + loc.top+")");
-            var ship = document.getElementById("Ship" + this.id);
-            ship.style.position = "absolute";
-            ship.style.top = loc.top + 'px';
-            ship.style.left = loc.left + 'px';
-            if (dir) {
-                ship.style.height = (50 * this.length) + 'px';
-                ship.style.width = 50 + 'px';
-            } else {
-                ship.style.height = 50 + 'px';
-                ship.style.width = (50 * this.length) + 'px';
-            }
-            ship.style.lineHeight=ship.style.height;
-        }
+
         // Auto run on create
         var ship = document.createElement("div");   // Create a <button> element
         ship.id = "Ship" + this.id;
-        ship.className="GamePiece";
-        ship.textContent=this.id;
+        ship.className = "GamePiece";
+        ship.textContent = this.id;
         document.body.appendChild(ship);
 
+    }
+    move(locationID, dir) {
+        this.direction = dir;
+        var loc = getOffset(locationID);
+        console.log("Moving Ship" + this.id + " to " + locationID + " (" + loc.left + ", " + loc.top + ")");
+        var ship = document.getElementById("Ship" + this.id);
+        ship.style.position = "absolute";
+        ship.style.top = loc.top + 'px';
+        ship.style.left = loc.left + 'px';
+        if (dir) {
+            ship.style.height = (50 * this.length) + 'px';
+            ship.style.width = 50 + 'px';
+        } else {
+            ship.style.height = 50 + 'px';
+            ship.style.width = (50 * this.length) + 'px';
+        }
+        ship.style.lineHeight = ship.style.height;
     }
     static incrementId() {
         if (this.latestId == null) this.latestId = 0;
@@ -55,6 +58,7 @@ class Ship {
         return this.latestId;
     }
 }
+// })();
 
 
 
