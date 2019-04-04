0f0cbbf480e93ab03bcb1e69682a1a928e02ec51
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-04 16:52:53 -0500

    Implemented drag and drop

--- js/Battleship.js
+++ js/Battleship.js
@@ -15,6 +15,24 @@
 // Useful functions
 //
 //
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+function drag(ev,id) {
+    console.log(ev.target.id);
+    ev.dataTransfer.setData("id", ev.target.id);
+    ev.dataTransfer.setData("shipID", id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var obj = ev.dataTransfer.getData("shipID");
+    console.log(ev.target.id);
+    console.log(obj);
+    Ship.playerShips[obj].move(ev.target.id,true);
+}
+
 function getOffset(el) {
     el = document.getElementById(el);
     const rect = el.getBoundingClientRect();
@@ -30,45 +48,68 @@ function getOffset(el) {
 // Game Building Functions
 //
 //
+let grid = document.getElementsByClassName("gridbox");
+for(let i=0;i<grid.length;i++){
+    grid[i].addEventListener('drop', function(event){drop(event)}, false);
+    grid[i].addEventListener('dragover', function(event){allowDrop(event)}, false);
+}
 class Ship {
     constructor(length) {
         this.id = Ship.incrementId();
         this.length = length;
         this.direction = true;
-
+        this.moved=false;
         // Auto run on create
-        var ship = document.createElement("div");   // Create a <button> element
-        ship.id = "Ship" + this.id;
-        ship.className = "GamePiece_Ship";
-        ship.textContent = this.id;
-        Ship.playerShips.push(ship);
-        document.body.appendChild(ship);
+        this.element=document.createElement("div");   // Create a <button> element;
+
+        this.element.id = "Ship" + this.id;
+        this.element.className = "GamePiece_Ship";
+        this.element.textContent = this.id;
+        this.element.draggable=true;
+        var myID = this.id;
+        this.element.addEventListener('dragstart', function(event){drag(event, myID)}, false);
+        Ship.playerShips.push(this);
+        document.body.appendChild(this.element);
 
     }
     move(locationID, dir) {
         this.direction = dir;
-        var loc = getOffset(locationID);
-        console.log("Moving Ship" + this.id + " to " + locationID + " (" + loc.left + ", " + loc.top + ")");
-        var ship = document.getElementById("Ship" + this.id);
-        ship.style.position = "absolute";
-        ship.style.top = (loc.top + 1) + 'px';
-        ship.style.left = (loc.left + 1) + 'px';
-        if (dir) {
-            ship.style.height = ((50 * this.length)-2) + 'px';
-            ship.style.width = 48 + 'px';
+        if (this.direction) {
+            this.element.style.height = ((50 * this.length)-2) + 'px';
+            this.element.style.width = 48 + 'px';
         } else {
-            ship.style.height = 48 + 'px';
-            ship.style.width = ((50 * this.length)-2) + 'px';
+            this.element.style.height = 48 + 'px';
+            this.element.style.width = ((50 * this.length)-2) + 'px';
         }
-        ship.style.lineHeight = ship.style.height;
+        if (this.moved){
+            this.element.style.transition ="all 0.1s ease";
+        }else{
+            this.element.style.top = "50%";
+            console.log("-"+(this.element.style.width));
+            this.element.style.left = "-"+(this.element.style.width);
+            this.element.style.transition ="all 1s ease";
+        }
+        var loc = getOffset(locationID);
+        this.element.style.position = "absolute";
+        this.element.style.top = (loc.top + 1) + 'px';
+        this.element.style.left = (loc.left + 1) + 'px';
+        this.element.style.lineHeight = this.element.style.height;
     }
-    static playerShips = []
+    static playerShips = [];
     static incrementId() {
         if (this.latestId == null) this.latestId = 0;
         else this.latestId++;
         return this.latestId;
     }
 }
+
+for(let i=0;i<5;i++){
+    new Ship(i+1);
+    (Ship.playerShips[i]).move("A"+(i+1),true);
+}
+
+
+
 // })();
 
 
