8bba0bf66d1ec371b34c2bf35224893b306f6f95
Ricky Davis <rdavis@Rickys-MacBook-Pro.local>
2019-04-22 11:53:33 -0500

    Added semicolons

--- js/handleGameOver.js
+++ js/handleGameOver.js
@@ -1,9 +1,9 @@
 function handleGameOver(){
     let gbCont = document.getElementById("PlayerGameBoard");
     if (gameWon){
-        gbCont.innerHTML="<h1>YOU WIN!</h1>"
+        gbCont.innerHTML="<h1>YOU WIN!</h1>";
         gbCont.style.backgroundColor="lightblue";
     }else{
-        gbCont.innerHTML="<h1>YOU LOSE!</h1>"
+        gbCont.innerHTML="<h1>YOU LOSE!</h1>";
     }
 }
